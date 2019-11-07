f5205997eefb1bc1998a59d0a7416d827eff090d
maxtnuk <maxtnuk@gmail.com>
2019-11-07 15:24:13 +0900

    Rmove extern keyword Add arr_macro to initialize array

--- art/src/lib.rs
+++ art/src/lib.rs
@@ -4,16 +4,6 @@
 #![warn(missing_debug_implementations)]
 
 #[macro_use]
-extern crate itertools;
-#[macro_use]
-extern crate static_assertions;
-extern crate crossbeam_epoch;
-extern crate crossbeam_utils;
-extern crate either;
-extern crate lock;
-extern crate rand;
-
-#[macro_use]
 mod utils;
 mod art;
 mod map;
