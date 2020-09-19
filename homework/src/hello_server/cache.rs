85722fd5f1a5e5383e370e7feeebbb91192aff35
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2020-09-19 12:17:45 +0000

    add tests for hw1

--- homework/src/hello_server/cache.rs
+++ homework/src/hello_server/cache.rs
@@ -26,3 +26,80 @@ impl<K: Eq + Hash + Clone, V: Clone> Cache<K, V> {
         todo!()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::Cache;
+    use crossbeam_channel::bounded;
+    use crossbeam_utils::thread::scope;
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::time::Duration;
+
+    const NUM_THREADS: usize = 8;
+    const NUM_KEYS: usize = 128;
+
+    #[test]
+    fn cache_no_duplicate_sequential() {
+        let cache = Cache::default();
+        cache.get_or_insert_with(1, |_| 1);
+        cache.get_or_insert_with(2, |_| 2);
+        cache.get_or_insert_with(3, |_| 3);
+        assert_eq!(cache.get_or_insert_with(1, |_| panic!()), 1);
+        assert_eq!(cache.get_or_insert_with(2, |_| panic!()), 2);
+        assert_eq!(cache.get_or_insert_with(3, |_| panic!()), 3);
+    }
+
+    #[test]
+    fn cache_no_duplicate_concurrent() {
+        for _ in 0..8 {
+            let cache = Cache::default();
+            // Count the number of times the computation is run.
+            let num_compute = AtomicUsize::new(0);
+            scope(|s| {
+                for _ in 0..NUM_THREADS {
+                    s.spawn(|_| {
+                        for key in 0..NUM_KEYS {
+                            cache.get_or_insert_with(key, |k| {
+                                num_compute.fetch_add(1, Ordering::Relaxed);
+                                k
+                            });
+                        }
+                    });
+                }
+            })
+            .unwrap();
+            assert_eq!(num_compute.load(Ordering::Relaxed), NUM_KEYS);
+        }
+    }
+
+    #[test]
+    fn cache_no_block_disjoint() {
+        let cache = &Cache::default();
+
+        scope(|s| {
+            // T1 blocks while inserting 1.
+            let (t1_quit_sender, t1_quit_receiver) = bounded(0);
+            s.spawn(move |_| {
+                cache.get_or_insert_with(1, |k| {
+                    t1_quit_receiver.recv().unwrap();
+                    k
+                });
+            });
+
+            // T2 must not be blocked by T1 when inserting 2.
+            let (t2_done_sender, t2_done_receiver) = bounded(0);
+            s.spawn(move |_| {
+                cache.get_or_insert_with(2, |k| k);
+                t2_done_sender.send(()).unwrap();
+            });
+
+            // If T2 is blocked, then recv will time out.
+            t2_done_receiver
+                .recv_timeout(Duration::from_secs(3))
+                .expect("Inserting a different key should not block");
+
+            t1_quit_sender.send(()).unwrap();
+        })
+        .unwrap();
+    }
+}
--- homework/src/hello_server/thread_pool.rs
+++ homework/src/hello_server/thread_pool.rs
@@ -17,8 +17,8 @@ struct Worker {
 }
 
 impl Drop for Worker {
-    /// When dropped, the thread should be `join`ed. NOTE that the thread is detached if not
-    /// `join`ed explicitly.
+    /// When dropped, the thread should be `join`ed.  If the worker panics, then this function should
+    /// panic too.  NOTE that the thread is detached if not `join`ed explicitly.
     fn drop(&mut self) {
         todo!()
     }
@@ -86,5 +86,77 @@ impl Drop for ThreadPool {
     /// When dropped, all worker threads must be properly `join`ed.
     fn drop(&mut self) {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::ThreadPool;
+    use crossbeam_channel::bounded;
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::sync::{Arc, Barrier};
+    use std::thread::sleep;
+    use std::time::Duration;
+
+    const NUM_THREADS: usize = 4;
+    const NUM_JOBS: usize = 1024;
+
+    #[test]
+    fn thread_pool_parallel() {
+        let pool = ThreadPool::new(NUM_THREADS);
+        let barrier = Arc::new(Barrier::new(NUM_THREADS));
+        let (done_sender, done_receiver) = bounded(NUM_THREADS);
+        for _ in 0..NUM_THREADS {
+            let barrier = barrier.clone();
+            let done_sender = done_sender.clone();
+            pool.execute(move || {
+                barrier.wait();
+                done_sender.send(()).unwrap();
+            });
+        }
+        for _ in 0..NUM_THREADS {
+            done_receiver.recv_timeout(Duration::from_secs(3)).unwrap();
+        }
+    }
+
+    // Run jobs that take NUM_JOBS milliseconds as a whole.
+    fn run_jobs(pool: &ThreadPool, counter: &Arc<AtomicUsize>) {
+        for _ in 0..NUM_JOBS {
+            let counter = counter.clone();
+            pool.execute(move || {
+                sleep(Duration::from_millis(NUM_THREADS as u64));
+                counter.fetch_add(1, Ordering::Relaxed);
+            });
+        }
+    }
+
+    /// `join` blocks until all jobs are finished.
+    #[test]
+    fn thread_pool_join_block() {
+        let pool = ThreadPool::new(NUM_THREADS);
+        let counter = Arc::new(AtomicUsize::new(0));
+        run_jobs(&pool, &counter);
+        pool.join();
+        assert_eq!(counter.load(Ordering::Relaxed), NUM_JOBS);
+    }
+
+    /// `drop` blocks until all jobs are finished.
+    #[test]
+    fn thread_pool_drop_block() {
+        let pool = ThreadPool::new(NUM_THREADS);
+        let counter = Arc::new(AtomicUsize::new(0));
+        run_jobs(&pool, &counter);
+        drop(pool);
+        assert_eq!(counter.load(Ordering::Relaxed), NUM_JOBS);
+    }
+
+    /// This indirectly tests if the worker threads are joined when the pool is dropped.
+    #[test]
+    #[should_panic]
+    fn thread_pool_drop_propagate_panic() {
+        let pool = ThreadPool::new(NUM_THREADS);
+        pool.execute(move || {
+            panic!();
+        });
     }
 }
