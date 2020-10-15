ee397d4c575b4d3bb276f38dc3fd619a2e8cb2ae
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2020-10-15 17:06:26 +0000

    forgot to add loom in arc skeleton

--- homework/src/arc.rs
+++ homework/src/arc.rs
@@ -7,7 +7,11 @@ use std::marker::PhantomData;
 use std::mem;
 use std::ops::Deref;
 use std::ptr::NonNull;
-use std::sync::atomic::{AtomicUsize, Ordering};
+
+#[cfg(feature = "check-loom")]
+use loom::sync::atomic::{fence, AtomicUsize, Ordering};
+#[cfg(not(feature = "check-loom"))]
+use std::sync::atomic::{fence, AtomicUsize, Ordering};
 
 const MAX_REFCOUNT: usize = (isize::MAX) as usize;
 
