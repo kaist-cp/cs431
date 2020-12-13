227f77391be543acb32997cea612f9ac6b279d1c
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2020-12-13 15:31:21 +0000

    note on hp fence

--- homework/src/hazard_pointer/mod.rs
+++ homework/src/hazard_pointer/mod.rs
@@ -18,6 +18,34 @@
 //! collect();
 //! ```
 //!
+//! # Algorithm and Synchronization
+//!
+//! Suppose a data structure has a memory block b. T1 wants to read the value written in b and T2
+//! wants to remove b from the data structure and free the memory. To prevent use-after-free, T1
+//! has to ensure that b is not freed before reading b and T2 has to check that no other threads
+//! are accessing b before freeing b. The hazard pointer library implements this mechanism as
+//! follows:
+//!
+//! ```text
+//! (T1-1) add b to the hazard list                   | (T2-1) unlink b (and retire b)
+//! (T1-2) check if b is still reachable (validation) | (T2-2) check if b is in the hazard list
+//!        if so, deref b                             |        if not, free b
+//! ```
+//!
+//! In the sequentially consistent memory model, there are 6 interleavings of each step.
+//! In any of them, either `T1-1 → T2-2` or `T2-1 → T1-2` holds.
+//! - If `T1-1 → T2-2`, then b is not freed, so no use-after-**free**
+//! - If `T2-1 → T1-2`, then the validation fails, so T1 will not read. No **use**-after-free.
+//!
+//! Therefore the algorithm is safe in sequentially consistent memory model. However, this is not
+//! true in relaxed memory model (construction of a counterexample is left as an exercise).
+//!
+//! To make the above reasoning sound in the relaxed memory model, we should use SC fence
+//! (`fence(SeqCst)`). Recall that SC fence joins the executing thread's view and the global view.
+//! So there is a total order among all SC fences and a SC fence happens-before another SC fence.
+//! If we insert a SC fence between `T1-1` and `T1-2`, and another between `T2-1` and `T2-2`, then
+//! either `T1's fence → T2's fence` or `T2's fence → T1's fence` holds.
+//! Therefore, `T1-1 → T2-2` or `T2-1 → T1-2`.
 use core::cell::RefCell;
 use std::sync::atomic::{fence, Ordering};
 use std::thread;
