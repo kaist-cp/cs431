63c604b1372933ec7e35e9e243575a5c93f8e446
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2021-12-10 11:08:21 +0900

    export rust nightly version variable
    fix #577

--- homework/scripts/grade-utils.sh
+++ homework/scripts/grade-utils.sh
@@ -9,7 +9,7 @@
 
 # rustup toolchain update stable nightly
 # TODO: https://github.com/rust-lang/rust/issues/91689
-NIGHTLY=2021-12-05
+export RUST_NIGHTLY=2021-12-05
 
 echo_err() {
     echo -e "\033[0;31m\033[1m$@\033[0m" 1>&2
@@ -47,7 +47,7 @@ cargo_asan() {
     local SUBCOMMAND=$1; shift
     RUSTFLAGS="-Z sanitizer=address" \
         RUSTDOCFLAGS="-Z sanitizer=address" \
-        cargo +nightly-$NIGHTLY $SUBCOMMAND --target x86_64-unknown-linux-gnu $@
+        cargo +nightly-$RUST_NIGHTLY $SUBCOMMAND --target x86_64-unknown-linux-gnu $@
 }
 export -f cargo_asan
 
@@ -57,7 +57,7 @@ cargo_tsan() {
         TSAN_OPTIONS="suppressions=suppress_tsan.txt" \
         RUSTDOCFLAGS="-Z sanitizer=thread" \
         RUST_TEST_THREADS=1 \
-        cargo +nightly-$NIGHTLY $SUBCOMMAND --target x86_64-unknown-linux-gnu $@
+        cargo +nightly-$RUST_NIGHTLY $SUBCOMMAND --target x86_64-unknown-linux-gnu $@
 }
 export -f cargo_tsan
 
